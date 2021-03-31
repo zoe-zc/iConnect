f48998028e3c1cabf81914efe76573b6268f60cb
tangxiaoqiang <xiao-qiang.tang@doone.com.cn>
2021-03-31 09:53:03 +0800

    @add 添加代码规范配置文件

--- code/src/views/Home/index.vue
+++ code/src/views/Home/index.vue
@@ -1,39 +1,38 @@
 <template>
-  <div class="page-view">
-      home
-  </div>
+    <div class="page-view">
+        home
+    </div>
 </template>
 <script>
 export default {
-  name: '',
-  mixins: [],
-  components: {},
-  // 页面数据部分
-  props: {},
-  data() {
-    return {}
-  },
-  computed: {},
-  watch: {},
-  // 页面生命周期
-  created() {},
-  mounted() {},
-  unmounted() {},
-  // 页面内方法
-  methods: {}
-}
+    name: "",
+    mixins: [],
+    components: {},
+    // 页面数据部分
+    props: {},
+    data() {
+        return {};
+    },
+    computed: {},
+    watch: {},
+    // 页面生命周期
+    created() {},
+    mounted() {},
+    unmounted() {},
+    // 页面内方法
+    methods: {},
+};
 </script>
 <style lang="scss" scoped>
-
 // pc端
 @media screen and (min-width: 992px) {
-    .page-view{
+    .page-view {
         font-size: 28px;
     }
 }
 // 移动端
 @media screen and (max-width: 992px) {
-    .page-view{
+    .page-view {
         font-size: 32px;
     }
 }
